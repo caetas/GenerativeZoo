b7987d609a5ed9b8298a62a9eba024d03db4c5df
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2024-06-28 15:30:50 +0200

    Cond Flow Matching

--- src/generativezoo/utils/util.py
+++ src/generativezoo/utils/util.py
@@ -134,6 +134,36 @@ def parse_args_FlowMatching():
     args.channel_scale_factors = tuple(args.channel_scale_factors)
     return args
 
+def parse_args_CondFlowMatching():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--train', action='store_true', default=False, help='train model')
+    argparser.add_argument('--sample', action='store_true', default=False, help='sample model')
+    argparser.add_argument('--batch_size', type=int, default=256, help='batch size')
+    argparser.add_argument('--n_epochs', type=int, default=100, help='number of epochs')
+    argparser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+    argparser.add_argument('--n_features', type=int, default = 64, help='number of features')
+    argparser.add_argument('--init_channels', type=int, default = 32, help='initial channels')
+    argparser.add_argument('--channel_scale_factors', type=int, nargs='+', default = [1, 2, 2], help='channel scale factors')
+    argparser.add_argument('--resnet_block_groups', type=int, default = 8, help='resnet block groups')
+    argparser.add_argument('--use_convnext', type=bool, default = True, help='use convnext (default: True)')
+    argparser.add_argument('--convnext_scale_factor', type=int, default = 2, help='convnext scale factor (default: 2)')
+    argparser.add_argument('--sample_and_save_freq', type=int, default=5, help='sample and save frequency')
+    argparser.add_argument('--dataset', type=str, default='mnist', help='dataset name', choices=['mnist', 'cifar10', 'cifar100', 'places365', 'dtd', 'fashionmnist', 'chestmnist', 'octmnist', 'tissuemnist', 'pneumoniamnist', 'svhn', 'tinyimagenet'])
+    argparser.add_argument('--checkpoint', type=str, default=None, help='checkpoint path')
+    argparser.add_argument('--num_samples', type=int, default=16, help='number of samples')
+    argparser.add_argument('--out_dataset', type=str, default='fashionmnist', help='outlier dataset name', choices=['mnist', 'cifar10', 'cifar100', 'places365', 'dtd', 'fashionmnist', 'chestmnist', 'octmnist', 'tissuemnist', 'pneumoniamnist', 'svhn', 'tinyimagenet'])
+    argparser.add_argument('--outlier_detection', action='store_true', default=False, help='outlier detection')
+    argparser.add_argument('--interpolation', action='store_true', default=False, help='interpolation')
+    argparser.add_argument('--solver_lib', type=str, default='torchdiffeq', help='solver library', choices=['torchdiffeq', 'zuko'])
+    argparser.add_argument('--step_size', type=float, default=0.1, help='step size for ODE solver')
+    argparser.add_argument('--solver', type=str, default='dopri5', help='solver for ODE', choices=['dopri5', 'rk4', 'dopri8', 'euler', 'bosh3', 'adaptive_heun', 'midpoint', 'explicit_adams', 'implicit_adams'])
+    argparser.add_argument('--num_classes', type=int, default=10, help='number of classes')
+    argparser.add_argument('--prob', type=float, default=0.5, help='probability of conditioning during training')
+    argparser.add_argument('--guidance_scale', type=float, default=2.0, help='guidance scale')
+    args = argparser.parse_args()
+    args.channel_scale_factors = tuple(args.channel_scale_factors)
+    return args
+
 def parse_args_VanillaFlow():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--train', action='store_true', default=False, help='train model')
