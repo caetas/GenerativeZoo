6ebabb9dd1a6bf73625ae893d017ebf3165cae88
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2024-10-16 10:03:54 +0200

    fixed batch norm

--- src/generativezoo/models/GAN/AdversarialVAE.py
+++ src/generativezoo/models/GAN/AdversarialVAE.py
@@ -59,7 +59,8 @@ class VanillaVAE(nn.Module):
             modules.append(
                 nn.Sequential(
                     nn.Conv2d(input_channels, h_dim, kernel_size = 3, stride = 2, padding = 1),
-                    nn.BatchNorm2d(h_dim),
+                    #nn.BatchNorm2d(h_dim),
+                    nn.GroupNorm(h_dim//2, h_dim),
                     nn.LeakyReLU()
                 )
             )
@@ -79,7 +80,8 @@ class VanillaVAE(nn.Module):
             modules.append(
                 nn.Sequential(
                     nn.ConvTranspose2d(hidden_dims[i], hidden_dims[i + 1], kernel_size=3, stride = 2, padding=1, output_padding=1),
-                    nn.BatchNorm2d(hidden_dims[i + 1]),
+                    #nn.BatchNorm2d(hidden_dims[i + 1]),
+                    nn.GroupNorm(hidden_dims[i + 1]//2, hidden_dims[i + 1]),
                     nn.LeakyReLU())
             )
         
@@ -87,7 +89,8 @@ class VanillaVAE(nn.Module):
 
         self.final_layer = nn.Sequential(
             nn.ConvTranspose2d(hidden_dims[-1], hidden_dims[-1], kernel_size=3, stride = 2, padding=1, output_padding=1),
-            nn.BatchNorm2d(hidden_dims[-1]),
+            #nn.BatchNorm2d(hidden_dims[-1]),
+            nn.GroupNorm(hidden_dims[-1]//2, hidden_dims[-1]),
             nn.LeakyReLU(),
             nn.Conv2d(hidden_dims[-1], out_channels=self.final_channels, kernel_size=3, padding=1),
             nn.Tanh()
@@ -210,14 +213,17 @@ class Discriminator(nn.Module):
             modules.append(
                 nn.Sequential(
                     nn.Conv2d(input_channels, h_dim, kernel_size = 3, stride = 2, padding = 1),
-                    nn.BatchNorm2d(h_dim),
+                    #nn.BatchNorm2d(h_dim),
+                    nn.GroupNorm(h_dim//2, h_dim),
                     nn.LeakyReLU()
                 )
             )
             input_channels = h_dim
 
         modules.append(nn.Flatten())
-        modules.append(nn.Linear(hidden_dims[-1]*(self.multiplier**2), 1))
+        modules.append(nn.Linear(hidden_dims[-1]*(self.multiplier**2), 1024))
+        modules.append(nn.LeakyReLU())
+        modules.append(nn.Linear(1024, 1))
         modules.append(nn.Sigmoid())
 
         self.encoder = nn.Sequential(*modules)
