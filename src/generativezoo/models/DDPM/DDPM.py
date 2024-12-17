dfeab4ac2cfd46f79f15c3ec1e4dfa48cd2b4428
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2024-12-17 10:41:44 +0100

    typo

--- src/generativezoo/models/DDPM/DDPM.py
+++ src/generativezoo/models/DDPM/DDPM.py
@@ -1000,7 +1000,6 @@ class DDPM(nn.Module):
                 optimizer.step()
                 acc_loss += loss.item() * batch_size
                 update_ema(self.ema, self.model, self.ema_rate)
-                break
 
             if not self.no_wandb:
                 accelerate.log({"Train Loss": acc_loss / len(dataloader.dataset)})
