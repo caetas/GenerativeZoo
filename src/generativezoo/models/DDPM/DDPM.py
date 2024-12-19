5bd3ca7de74cfacedb2e32d9bd35a39b4a3dff46
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2024-12-19 17:03:16 +0100

    autocast

--- src/generativezoo/models/DDPM/DDPM.py
+++ src/generativezoo/models/DDPM/DDPM.py
@@ -982,18 +982,18 @@ class DDPM(nn.Module):
             self.model.train()
             acc_loss = 0.0
             for batch in tqdm(dataloader, desc=f'Batches', leave=False, disable=not verbose):
+                optimizer.zero_grad()
+                batch_size = batch[0].shape[0]
+                batch = batch[0].to(self.device)
+
+                if self.vae is not None:
+                    with torch.no_grad():
+                        # if x has one channel, make it 3 channels
+                        if batch.shape[1] == 1:
+                            batch = torch.cat((batch, batch, batch), dim=1)
+                        batch = self.vae.module.encode(batch).latent_dist.sample().mul_(0.18215)
+
                 with accelerate.autocast():
-                    optimizer.zero_grad()
-                    batch_size = batch[0].shape[0]
-                    batch = batch[0].to(self.device)
-
-                    if self.vae is not None:
-                        with torch.no_grad():
-                            # if x has one channel, make it 3 channels
-                            if batch.shape[1] == 1:
-                                batch = torch.cat((batch, batch, batch), dim=1)
-                            batch = self.vae.module.encode(batch).latent_dist.sample().mul_(0.18215)
-
                     t = torch.randint(0, self.timesteps, (batch_size,), device=self.device).long()
                     loss = self.criterion(forward_diffusion_model=self.forward_diffusion_model, denoising_model=self.model, x_start=batch, t=t, loss_type=self.loss_type)
                     accelerate.backward(loss)
@@ -1018,7 +1018,7 @@ class DDPM(nn.Module):
 
                 if self.vae is not None:
                     with torch.no_grad():
-                        all_images = self.vae.decode(all_images.to(self.device) / 0.18215).sample 
+                        all_images = self.vae.module.decode(all_images.to(self.device) / 0.18215).sample 
                         all_images = all_images.cpu().detach()
 
                 all_images = all_images * 0.5 + 0.5
