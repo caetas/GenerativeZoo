efcebc904d8b2f8c4ebe18ebce924462e6c96c0f
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2025-11-28 12:23:50 +0100

    some fixes

--- src/generativezoo/models/FM/JiT.py
+++ src/generativezoo/models/FM/JiT.py
@@ -148,7 +148,9 @@ class VisionRotaryEmbeddingFast(nn.Module):
             self.freqs_cos = freqs.cos().view(-1, freqs.shape[-1]).cuda()
             self.freqs_sin = freqs.sin().view(-1, freqs.shape[-1]).cuda()
 
-    def forward(self, t): return  t * self.freqs_cos + rotate_half(t) * self.freqs_sin
+    def forward(self, t): 
+        print(t.device, self.freqs_cos.device)
+        return  t * self.freqs_cos + rotate_half(t) * self.freqs_sin
 
 
 class RMSNorm(nn.Module):
@@ -557,7 +559,6 @@ class JiT(nn.Module):
                 in_context_tokens = y_emb.unsqueeze(1).repeat(1, self.in_context_len, 1)
                 in_context_tokens += self.in_context_posemb
                 x = torch.cat([in_context_tokens, x], dim=1)
-            print(x.device, c.device)
             x = block(x, c, self.feat_rope if i < self.in_context_start else self.feat_rope_incontext)
 
         x = x[:, self.in_context_len:]
