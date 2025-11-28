be4bce56012b63f99d4484edc4c5aec7cb882f86
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2025-11-28 12:27:17 +0100

    some fixes

--- src/generativezoo/models/FM/JiT.py
+++ src/generativezoo/models/FM/JiT.py
@@ -142,14 +142,12 @@ class VisionRotaryEmbeddingFast(nn.Module):
             cos_pad = torch.ones(num_cls_token, D, dtype=cos_img.dtype, device=cos_img.device)
             sin_pad = torch.zeros(num_cls_token, D, dtype=sin_img.dtype, device=sin_img.device)
 
-            self.freqs_cos = torch.cat([cos_pad, cos_img], dim=0).cuda()  # [N_cls+N_img, D]
-            self.freqs_sin = torch.cat([sin_pad, sin_img], dim=0).cuda()
+            self.freqs_cos = torch.cat([cos_pad, cos_img], dim=0).to(cos_img.device)  # [N_cls+N_img, D]
+            self.freqs_sin = torch.cat([sin_pad, sin_img], dim=0).to(sin_img.device)
         else:
-            self.freqs_cos = freqs.cos().view(-1, freqs.shape[-1]).cuda()
-            self.freqs_sin = freqs.sin().view(-1, freqs.shape[-1]).cuda()
-
+            self.freqs_cos = freqs.cos().view(-1, freqs.shape[-1]).to(freqs.device)
+            self.freqs_sin = freqs.sin().view(-1, freqs.shape[-1]).to(freqs.device)
     def forward(self, t): 
-        print(t.device, self.freqs_cos.device)
         return  t * self.freqs_cos + rotate_half(t) * self.freqs_sin
 
 
